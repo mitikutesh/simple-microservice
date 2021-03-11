3db634fcd8bf3e9c229a05b851e6f5ffad03927a
Mitiku Geleta <mitiku.geleta@tuni.fi>
2021-03-11 22:54:36 +0200

    simple working ocelot gateway settings

--- SimpleMicroService/APIGateway/Program.cs
+++ SimpleMicroService/APIGateway/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace APIGateway
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -20,7 +20,11 @@ namespace APIGateway
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
                     webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder.ConfigureAppConfiguration(config =>
+                        config.AddJsonFile($"ocelot.{env}.json"));
+                })
+            .ConfigureLogging(logging => logging.AddConsole());  
     }
 }
