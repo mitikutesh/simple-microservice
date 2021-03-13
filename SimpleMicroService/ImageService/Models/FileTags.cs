aac8aa33410d2a773bfc32c47cb108f3bc4a2e9a
Mitiku Geleta <mitiku.geleta@tuni.fi>
2021-03-13 11:41:33 +0200

    add model

---
+++ SimpleMicroService/ImageService/Models/FileTags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageService.Models
+{
+    public class FileTags
+    {
+        public Guid Id { get; set; }
+        public Guid FileId { get; set; }
+        public Guid TagId { get; set; }
+    }
+}
